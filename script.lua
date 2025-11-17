1acb86f44cab7bc4c1ad2212eb420f9c07635f75
arabnik <hyperbot228@gmail.com>
2025-11-17 19:00:54 +0300

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,37 @@
+cd = 0.0002 -- adjust the speed as you need(in seconds)
+
+-- getting weapon(DON'T TOUCH IT!)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Weapons["AK-47"].Hitbox.CFrame
+wait(cd)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Weapons["AN-94"].Hitbox.CFrame
+wait(cd)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Weapons["AWP"].Hitbox.CFrame
+wait(cd)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Weapons["Colt Anaconda"].Hitbox.CFrame
+wait(cd)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Weapons["DB Shotgun"].Hitbox.CFrame
+wait(cd)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Weapons["Desert Eagle"].Hitbox.CFrame
+wait(cd)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Weapons["Flamethrower"].Hitbox.CFrame
+wait(cd)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Weapons["G36C"].Hitbox.CFrame
+wait(cd)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Weapons["M1014"].Hitbox.CFrame
+wait(cd)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Weapons["M14"].Hitbox.CFrame
+wait(cd)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Weapons["M16A2/M203"].Hitbox.CFrame
+wait(cd)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Weapons["M4A1"].Hitbox.CFrame
+wait(cd)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Weapons["MP5k"].Hitbox.CFrame
+wait(cd)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Weapons["P90"].Hitbox.CFrame
+wait(cd)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Weapons["R870"].Hitbox.CFrame
+wait(cd)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Weapons["RayGun"].Hitbox.CFrame
+wait(cd)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Weapons["SVD"].Hitbox.CFrame
